e8285f41d840a368d7764c64e6d19d272514d63e
Reenakaushal <reenakaushal663@gmail.com>
2024-02-19 18:00:28 +0530

    2

--- function.js
+++ function.js
@@ -8,6 +8,8 @@ function add(a,b){
  add(873 , 34);
  
  function add(a,b){
+
+
     var  c= a+b;
      console.log(c);
  }
@@ -17,4 +19,37 @@ function add(a,b){
  
 
 
- 
+ // CONSOLE.LOG(C);
+
+ const getAvg = function(m1,m2,m3){
+    const avg = (m1+m2+m3)/3;
+
+    // console.log(avg);
+ }
+
+  const result = getAvg(34,56,78);
+
+
+console.log(result);
+
+
+
+const getFact = (n) =>{
+    let f = 1;
+    for(let i=1; i<=n; i++){
+        f=f*i;
+    }
+}
+  getFact(5);
+
+
+console.log(f);
+return f;
+}
+
+getFact(5);
+
+
+
+
+//const myRes
